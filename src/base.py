41407d950fe0a6fd14091e55c045de5a361ba039
m-310101 <marlon.miguel@bath.edu>
2024-01-22 00:17:49 +0100

    added board class with cli outputting

--- src/base.py
+++ src/base.py
@@ -13,5 +13,74 @@ If you want to replace this with a Flask application run:
 and then choose `flask` as template.
 """
 
-# example constant variable
-NAME = "sudoku_ai"
+from sudoku import Sudoku
+import random
+
+class SudokuBoard:
+    # initialise the board, default size is 3x3 and default difficulty is 0.5
+    def __init__(self, size=3, difficulty=0.5):
+        self.size = size
+        self.difficulty = difficulty
+        self.puzzle = self.generateBoard()
+
+    def generateBoard(self):
+        return Sudoku(self.size).difficulty(self.difficulty)
+    
+    def updateBoard(self):
+        self.puzzle = self.generateBoard()
+
+    def getBoard(self):
+        return self.puzzle
+
+    def getBoardArray(self):
+        return self.puzzle.board
+    
+    def getBoardSize(self):
+        return self.size
+    
+    def getBoardDifficulty(self):
+        return self.difficulty
+    
+    def setBoardSize(self, size):
+        self.size = size
+
+    def setBoardDifficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def printBoard(self):
+        self.puzzle.show()
+
+    def getBoardCell(self, row, col):
+        return self.puzzle.board[row][col]
+    
+    def setBoardCell(self, row, col, value):
+        self.puzzle.board[row][col] = value
+
+    def getBoardRow(self, row):
+        return self.puzzle.board[row]
+    
+    def getBoardCol(self, col):
+        return [row[col] for row in self.puzzle.board]
+    
+    def getBoardBox(self, row, col):
+        box = []
+        for i in range(self.size):
+            for j in range(self.size):
+                box.append(self.puzzle.board[row + i][col + j])
+        return box
+    
+    def getBoardEmptyCells(self):
+        emptyCells = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.puzzle.board[i][j] is None:
+                    emptyCells.append((i, j))
+        return emptyCells
+    
+    def getBoardEmptyCellsCount(self):
+        return len(self.getBoardEmptyCells())
+    
+    def getBoardEmptyCellsRandom(self):
+        return random.choice(self.getBoardEmptyCells())
+    
+
--- src/cli.py
+++ src/cli.py
@@ -8,21 +8,43 @@ Be creative! do whatever you want!
 - Import things from your .base module
 """
 
+from .base import SudokuBoard
 
 def main():  # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
-
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
     print("This will do something")
+    clarence = SudokuBoard(3, 0.7)
+
+    print('\ngetBoard()')
+    print(clarence.getBoard())
+    print('\ngetBoardArray()')
+    print(clarence.getBoardArray())
+    print('\ngetBoardSize()')
+    print(clarence.getBoardSize())
+    print('\ngetBoardDifficulty()')
+    print(clarence.getBoardDifficulty())
+    print('\nsetBoardSize(4)')
+    print(clarence.setBoardSize(4))
+    print('\nsetBoardDifficulty(0.4)')
+    print(clarence.setBoardDifficulty(0.4))
+    print('\nupdateBoard()')
+    print(clarence.updateBoard())
+    print('\ngetBoard()')
+    print(clarence.getBoard())
+    print('\nsetBoardCell(3, 5, 12)')
+    print(clarence.setBoardCell(3, 5, 12))
+    print('\ngetBoardCell(3, 5)')
+    print(clarence.getBoardCell(3, 5))
+    print('\ngetBoard()')
+    print(clarence.getBoard())
+    print('\ngetBoardRow(4)')
+    print(clarence.getBoardRow(4))
+    print('\ngetBoardCol(2)')
+    print(clarence.getBoardCol(2))
+    print('\ngetBoardBox(4, 0)')
+    print(clarence.getBoardBox(4, 0))
+    print('\ngetBoardEmptyCells()')
+    print(clarence.getBoardEmptyCells())
+    print('\ngetBoardEmptyCellsCount()')
+    print(clarence.getBoardEmptyCellsCount())
+    print('\ngetBoardEmptyCellsRandom()')
+    print(clarence.getBoardEmptyCellsRandom())
