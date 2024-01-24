6a575aa41eea429247e6ca5bc173dff439479f55
m-310101 <marlon.miguel@bath.edu>
2024-01-24 23:51:00 +0100

    Altered printing

--- src/cli.py
+++ src/cli.py
@@ -2,7 +2,6 @@ from .base import SudokuBoard, SudokuSolver
 
 
 def main():  # pragma: no cover
-    print("This will do something")
     board = SudokuBoard(3, 0.5)
     board.print_board()
     solver = SudokuSolver(board)
